12c3983fb2e9e03cb4f834aecaeef6e12b807b1c
Maul <rizamaulana1204@gmail.com>
2025-03-20 16:37:07 +0700

    Add files via upload

---
+++ 105223045_Rizamaulana_LP4.java
@@ -0,0 +1,36 @@
+class Mahasiswa {
+    String nama;
+    String nim;
+    String jurusan;
+    double ipk;
+
+    public Mahasiswa(String nama, String nim, String jurusan, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+        this.ipk = ipk;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Nama     : " + nama);
+        System.out.println("NIM      : " + nim);
+        System.out.println("Jurusan  : " + jurusan);
+        System.out.println("IPK      : " + ipk);
+    }
+
+    public boolean cekLulus() {
+        return ipk >= 3.0;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa("Hilmi Hidayat", "2547", "Teknik Informatika", 3.5);
+        mhs1.tampilkanInfo();
+        if (mhs1.cekLulus()) {
+            System.out.println("Status   : Lulus");
+        } else {
+            System.out.println("Status   : Tidak Lulus");
+        }
+    }
+}
