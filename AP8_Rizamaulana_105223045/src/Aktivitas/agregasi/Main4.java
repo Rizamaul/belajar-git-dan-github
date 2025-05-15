a906f83c32a08e2ca69743d549bac33a634f354c
Maul <rizamaulana1204@gmail.com>
2025-05-15 16:27:26 +0700

    Add files via upload

---
+++ AP8_Rizamaulana_105223045/src/Aktivitas/agregasi/Main4.java
@@ -0,0 +1,43 @@
+package Aktivitas.agregasi;
+import java.util.ArrayList;
+
+class Mahasiswa {
+    String nama;
+
+    Mahasiswa(String nama){
+        this.nama = nama;
+    }
+}
+
+class Universitas {
+    String namaUniv;
+    ArrayList<Mahasiswa> daftarMahasiswa = new ArrayList<>();
+
+    Universitas(String namaUniv){
+        this.namaUniv = namaUniv;
+    }
+
+    void tambahMahasiswa(Mahasiswa mahasiswa){
+        daftarMahasiswa.add(mahasiswa);
+    }
+
+    void tampilkanMahasiswa(){
+        for (Mahasiswa mahasiswa : daftarMahasiswa) {
+            System.out.println(mahasiswa.nama);
+        }
+    }
+}
+
+public class Main4 {
+    
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa("radit");
+        Mahasiswa mhs2 = new Mahasiswa("bandit");
+
+        Universitas uper = new Universitas("Universitas Pertamina");
+        uper.tambahMahasiswa(mhs1);
+        uper.tambahMahasiswa(mhs2);
+
+        uper.tampilkanMahasiswa();
+    }
+}
